3980d7a88c3d6dd1b6a28a051e416415b50ff05d
Champion113 <sbalbright19@gmail.com>
2021-01-17 13:17:26 -0500

    clone code

---
+++ client/src/pages/Home.js
@@ -0,0 +1,28 @@
+import ThoughtList from '../components/ThoughtList';
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
+
+const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+  return (
+    <main>
+  <div className="flex-row justify-space-between">
+    <div className="col-12 mb-3">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+      )}
+    </div>
+  </div>
+</main>
+  );
+  const thoughts = data?.thoughts || [];
+console.log(thoughts);
+};
+
+export default Home;
